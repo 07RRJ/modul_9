e125fc7de8552ef5ee6412b574175af98d7845bf
07RRJ <rasmusrudko07@gmail.com>
2025-03-24 14:36:26 +0100

    1.1

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from sys import exit
 from time import sleep
 from random import randint
 from msvcrt import getwch
+from colors import bcolors
 
 # import color
 
@@ -12,20 +13,8 @@ function_ = ""
 first_number = 0
 second_number = 0
 
-def add(first_number, second_number):
-    return first_number + second_number
-
-def subtract(first_number, second_number):
-    return first_number - second_number
-
-def multiply(first_number, second_number):
-    return first_number * second_number
-
-def devide(first_number, second_number):
-    return first_number / second_number
-
 while True:
-    print("1) add\n2) subtract\n3) multiply\n4) divide")
+    print(bcolors.DEFAULT + "1) add\n2) subtract\n3) multiply\n4) divide")
     function_ = getwch()
     if function_ == "q":
         os.system("cls")
@@ -39,14 +28,14 @@ while True:
 
             while True:
                 try:
-                    first_number = float(input("first number: "))
+                    first_number = input("first number: ")
                     break
                 except:
                     os.system("cls")
                     print("use a valid number")
             while True:
                 try:
-                    second_number = float(input("second number: "))
+                    second_number = input("second number: ")
                     break
                 except:
                     os.system("cls")
@@ -56,26 +45,28 @@ while True:
             print("thinking")
             for repets in range(1,4):
                 for dots in range(1,4):
-                    sleep((randint(1, 4))/10)
+                    sleep((randint(1, 2))/10)
                     os.system("cls")
                     print("thinking"+"." * dots)
             sleep(0.5)
-            os.system("cls")
-
-            if function_ == 1:
-                print(f"{first_number} + {second_number} = {add(first_number, second_number)}\n")
-            elif function_ == 2:
-                print(f"{first_number} - {second_number} = {subtract(first_number, second_number)}\n")
-            elif function_ == 3:
-                print(f"{first_number} * {second_number} = {multiply(first_number, second_number)}\n")
-            elif function_ == 4:
-                if second_number == 0:
-                    print("you cant devide by \"0\"")
-                else:
-                    print(f"{first_number} / {second_number} = {devide(first_number, second_number)}\n")
-            
-            else:
-                print("use a valid input\n")
+            try:
+                print(bcolors.GREEN)
+                os.system("cls")
+                if function_ == 1:
+                    print(f"{first_number} + {second_number} = {eval((first_number) + (second_number))}\n")
+                elif function_ == 2:
+                    print(f"{first_number} - {second_number} = {eval((first_number) - (second_number))}\n")
+                elif function_ == 3:
+                    print(f"{first_number} * {second_number} = {eval((first_number) * (second_number))}\n")
+                elif function_ == 4:
+                    if second_number == 0:
+                        print("you cant devide by \"0\"")
+                    else:
+                        print(f"{first_number} / {second_number} = {eval((first_number) / (second_number))}\n")
+                bcolors.DEFAULT
+            except:
+                os.system("cls")
+                print(bcolors.RED,"use a valid input\n",bcolors.DEFAULT)
         else:
             print("chose a number available\n")
     else:
--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+import os
+os.system("cls")
+
+
+
 # import platform
 
 # x = platform.system()
@@ -8,14 +13,45 @@
 
 # print(int(float('68.0')))
 
-import msvcrt
 
-print("Press 'Esc' to exit the program.")
 
-while True:
-    key = msvcrt.getwch()  # Wait for a key press
-    if key == 'q':  # Escape key in ASCII
-        print("Exiting the program...")
-        break  # Exit the loop
-    else:
-        print(f"You pressed: {key}")
+# import msvcrt
+
+# while True:
+#     print("Press 'q' to exit the program.")
+#     key = msvcrt.getwch()  # Wait for key press
+#     if key == 'q':
+#         print("Exiting the program...")
+#         break  # Exit the loop
+#     else:
+#         print(f"You pressed: {key}")
+
+
+
+# variable = "Hello_werld"
+# test = variable.split("e")
+# print(test)
+# test = variable.split("e")[1]
+# print(test)
+
+
+
+# def count_decimals(number):
+#     str_number = str(number)
+#     if "." in str_number:
+#         decimal_part = str_number.split(".")[1]
+#         return len(decimal_part)
+#     else:
+#         return 0    
+
+# my_float = float(input(":"))
+# decimal_count = count_decimals(my_float)
+# print(f"The number {my_float} has {decimal_count} decimal places.")
+
+
+# def remove_decimals(number, decimal_places):
+#     return round(number, decimal_places)
+
+# # Example usage
+# rounded_float = remove_decimals(my_float, 2)  # Round to 2 decimal places
+# print(f"The number {my_float} rounded to 2 decimal places is: {rounded_float}")
